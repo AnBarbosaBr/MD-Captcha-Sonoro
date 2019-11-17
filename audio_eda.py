1a8d94c3f985dcab36a08259a5022484c4de8aa9
André Barbosa <anbarbosa.br@gmail.com>
2019-11-17 12:16:08 -0300

    Reworked some functions

--- audio_eda.py
+++ audio_eda.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[34]:
+
+
 # Importings base libraries
 import os;
 import pandas as pd; 
@@ -6,56 +12,73 @@ import librosa.display
 import IPython.display as ipd
 import matplotlib.pyplot as plt
 
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[35]:
+
+
 # Input Variables and Setup
 training_data_path   = os.path.join(".","dados","TREINAMENTO")
 validation_data_path = os.path.join(".","dados","VALIDACAO")
 
-# Helper Functions
-def get_files_from(path):
-    from os.path import isfile, join
-    from os import listdir
-    onlyfiles = [join(path,f) for f in listdir(path) if (isfile(join(path, f)) and f.endswith(".wav")) ]
-    return onlyfiles
 
-def load_wav_file(file, intervalo_segundos = 2):
-    audio_data, sampling_rate = librosa.load(file, None)
-    duracao = audio_data.shape[0]/sampling_rate
-    frames_por_audio = sampling_rate * intervalo_segundos
-    dados_por_amostra = list()
-    for i, ini in enumerate(range(0, audio_data.shape[0], frames_por_audio)):
-        dados_por_amostra.append(pd.Series(audio_data[ini:(ini+frames_por_audio)]))
-    
-    return(dados_por_amostra)
+# In[69]:
 
-def get_labels(file):
-    base = os.path.basename(file)
-    without_extension = os.path.splitext(base)[0]
-    return without_extension
 
-def get_sampling_rate(file):
-    return librosa.load(file, None)[1]
+def get_audio_and_sampling_rate_from_df(df, row_index, asNumpyArray = True ):
+    data, sampling_rate, label = get_data_sample_rate_and_legend_from_df(df, row_index, asNumpyArray)
+    return(data, sampling_rate)
 
-def join_onda_with_label(waves, labels, sampling_rate, duracao_minima = 2.0, duracao_maxima = 2.0):
-    labs = pd.Series(list(labels))
-    ondas = pd.DataFrame(waves)
-    duracoes = [ w.shape[0]/sr for w in waves]
-    ondas.insert(loc  = 0, column = 'label', value = labs)
-    ondas.insert(loc  = 1, column = 'duracao', value = duracoes)
-    ondas.insert(loc = 2, column = 'sr', value = sampling_rate )
-    ondas = ondas[ (ondas['duracao'] >= duracao_minima) & (ondas['duracao'] <= duracao_maxima) ]
-    return ondas
-
-def get_audio_and_sr_from_df(df, row_index, sample_rate_col_index = 2, first_data_col_index = 3, asNumpyArray = True ):
-    data = df.iloc[row_index, first_data_col_index: ].astype('float64')
+def get_data_sampling_rate_and_legend_from_df(df, row_index, asNumpyArray = True):
+    data = df.iloc[ row_index , df.columns.get_loc(0): ].astype('float64')
     if(asNumpyArray):
         data = data.values
-    sample_rate = df.iloc[row_index, sample_rate_col_index]
-    return(data, sample_rate)
+    sample_rate = df.loc[: , "sr"].iloc[row_index]
+    label = df.loc[ : , 'label'].iloc[row_index]
+    return(data, sample_rate, label)
+    
+def show_data(df, row):
+    # Retrieve information from DF
+    audio_data, sampling_rate, label = get_data_sample_rate_and_legend_from_df(df, row)
+    
+    # Print some stats and display the sound
+    print(f"{label}({librosa.get_duration(audio_data, sr=sampling_rate)} sec)")
+    ipd.display(ipd.Audio(audio_data, rate=sampling_rate))
+    
+    print("\n")
+    # Make plots
+    X = librosa.stft(audio_data)
+    Xdb = librosa.amplitude_to_db(abs(X))
 
-# Loading Files
+    plt.figure(figsize=(8, 16), dpi= 80, facecolor='w', edgecolor='k')
+
+    plt.subplot(3, 1, 1)
+    plt.title("Wave")
+    librosa.display.waveplot(audio_data, sr=sampling_rate, x_axis="time")
+
+    plt.subplot(3, 1, 2)
+    plt.title("MEL")
+    librosa.display.specshow(Xdb, sr=sampling_rate, x_axis="time", y_axis="mel")
+
+    plt.subplot(3, 1, 3)
+    plt.title("HZ")
+    librosa.display.specshow(Xdb, sr=sampling_rate, x_axis="time", y_axis="hz")
+
+    print("Audio")
+    ipd.Audio(audio_data, rate = sampling_rate)
+
+
+# In[62]:
+
+
 f = get_files_from(training_data_path)
 
-# Getting one as example
+
+# In[63]:
+
+
 example = f[1]
 
 wave = load_wav_file(example)
@@ -64,16 +87,34 @@ labels = get_labels(example)
 
 df = join_onda_with_label(wave, labels, sr)
 
-# Get information about one of the files on the df
+
+# In[64]:
+
+
 data, sampling_rate = get_audio_and_sr_from_df(df, 1)
 
-# Get duration of file
+
+# In[50]:
+
+
+data
+
+
+# In[66]:
+
+
 librosa.get_duration(data, sr=sampling_rate)
 
-# Listen(on IPython)
+
+# In[67]:
+
+
 ipd.Audio(data, rate=sampling_rate)
 
-# Plots 
+
+# In[68]:
+
+
 X = librosa.stft(data)
 Xdb = librosa.amplitude_to_db(abs(X))
 
@@ -90,6 +131,10 @@ librosa.display.specshow(Xdb, sr=sampling_rate, x_axis="time", y_axis="mel")
 plt.subplot(3, 1, 3)
 plt.title("HZ")
 librosa.display.specshow(Xdb, sr=sampling_rate, x_axis="time", y_axis="hz")
+
+
+# In[ ]:
+
 
 
 
--- audio_ingestion.py
+++ audio_ingestion.py
@@ -24,9 +24,10 @@ training_data_path   = os.path.join(".","dados","TREINAMENTO")
 validation_data_path = os.path.join(".","dados","VALIDACAO")
 
 
-# In[44]:
+# In[ ]:
 
 
+# Loading Functions - Those functions are helpers to ingest the data
 def get_files_from(path):
     from os.path import isfile, join
     from os import listdir
@@ -43,15 +44,6 @@ def load_wav_file(file, intervalo_segundos = 2):
     
     return(dados_por_amostra)
 
-def get_labels(file):
-    base = os.path.basename(file)
-    without_extension = os.path.splitext(base)[0]
-    return without_extension
-
-
-def get_sampling_rate(file):
-    return librosa.load(file, None)[1]
-
 def join_onda_with_label(waves, labels, sampling_rate, duracao_minima = 2.0, duracao_maxima = 2.0):
     labs = pd.Series(list(labels))
     ondas = pd.DataFrame(waves)
@@ -63,23 +55,11 @@ def join_onda_with_label(waves, labels, sampling_rate, duracao_minima = 2.0, dur
     ondas = ondas[ (ondas['duracao'] >= duracao_minima) & (ondas['duracao'] <= duracao_maxima) ]
     return ondas
 
-def get_audio_and_sr_from_df(df, row_index, sample_rate_col_index = 2, first_data_col_index = 4, asNumpyArray = True ):
-    data = df.iloc[row_index, first_data_col_index: ].astype('float64')
-    if(asNumpyArray):
-        data = data.values
-    sample_rate = df.iloc[row_index, sample_rate_col_index]
-    return(data, sample_rate)
-
-
-# In[45]:
-
-
-f = get_files_from(training_data_path)
-
 
 # In[46]:
 
 
+f = get_files_from(training_data_path)
 lista_df = list()
 processed = 0
 to_be = len(f)
@@ -96,31 +76,25 @@ for file in f:
 # In[47]:
 
 
+
 df = pd.concat(lista_df, ignore_index=True)
-
-
-# In[48]:
-
-
 df.to_pickle("training_data.pickle")
 
 
-# In[54]:
+# In[1]:
 
 
-df.shape
-
-
-# In[65]:
-
-
-len(df.original_file.unique()) 
+# Verificando data frame
+print(f"Shape: {df.shape} \n"+
+      f"Número de Audios: {df.shape[0]} \n"+
+      f"Número de Audios/4: {df.shape[0]/4}\n" +
+      f"Numero De Arquivos Lidos = {len(df.original_file.unique())}\n")
 
 
 # In[115]:
 
 
-grupos = df.groupby("original_file").sr.count().sort_values(0)
+# Verificando quais grupos = df.groupby("original_file").sr.count().sort_values(0)
 grupo_problemas = grupos[grupos < 4] 
 grupo_problemas
 
@@ -144,18 +118,74 @@ grupo_problemas
 # Name: sr, dtype: int64
 
 
-# In[117]:
+# In[120]:
 
 
-# Verificando os que estão no DF - realmente a ultima letra não aparece.
-df[[v in grupo_problemas  for v in df["original_file"]]]
+f = get_files_from(validation_data_path)
+lista_df_validation = list()
+processed = 0
+to_be = len(f)
+for file in f:
+    print(f"{file}: Processing {processed} of {to_be}.")
+    wave = load_wav_file(file)
+    sr = get_sampling_rate(file)
+    labels = get_labels(file)
+    temp_df = join_onda_with_label(wave, labels, sr)
+    lista_df_validation.append(temp_df)
+    processed += 1
 
 
-# In[119]:
+# In[122]:
 
 
-# Verificando se podemos obter os dados de audio de uma linha qualquer
-data, sr = get_audio_and_sr_from_df(df, 1409)
-print(df.label[1409])
-ipd.Audio(data, rate=sr)
+df_val = pd.concat(lista_df, ignore_index=True)
+df_val.to_pickle("validation.pickle")
+
+
+# In[44]:
+
+
+# Working with data - Those functions are helpers to work with the final DataFrame
+
+def get_audio_and_sampling_rate_from_df(df, row_index, asNumpyArray = True ):
+    data, sampling_rate, label = get_data_sample_rate_and_legend_from_df(df, row_index, asNumpyArray)
+    return(data, sampling_rate)
+
+def get_data_sampling_rate_and_legend_from_df(df, row_index, asNumpyArray = True):
+    data = df.iloc[ row_index , df.columns.get_loc(0): ].astype('float64')
+    if(asNumpyArray):
+        data = data.values
+    sample_rate = df.loc[: , "sr"].iloc[row_index]
+    label = df.loc[ : , 'label'].iloc[row_index]
+    return(data, sample_rate, label)
+    
+def show_data(df, row):
+    # Retrieve information from DF
+    audio_data, sampling_rate, label = get_data_sample_rate_and_legend_from_df(df, row)
+    
+    # Print some stats and display the sound
+    print(f"{label}({librosa.get_duration(audio_data, sr=sampling_rate)} sec)")
+    ipd.display(ipd.Audio(audio_data, rate=sampling_rate))
+    
+    print("\n")
+    # Make plots
+    X = librosa.stft(audio_data)
+    Xdb = librosa.amplitude_to_db(abs(X))
+
+    plt.figure(figsize=(8, 16), dpi= 80, facecolor='w', edgecolor='k')
+
+    plt.subplot(3, 1, 1)
+    plt.title("Wave")
+    librosa.display.waveplot(audio_data, sr=sampling_rate, x_axis="time")
+
+    plt.subplot(3, 1, 2)
+    plt.title("MEL")
+    librosa.display.specshow(Xdb, sr=sampling_rate, x_axis="time", y_axis="mel")
+
+    plt.subplot(3, 1, 3)
+    plt.title("HZ")
+    librosa.display.specshow(Xdb, sr=sampling_rate, x_axis="time", y_axis="hz")
+
+    print("Audio")
+    ipd.Audio(audio_data, rate = sampling_rate)
 
