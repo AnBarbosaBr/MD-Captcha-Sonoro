592e987bf2440d1487ee38926966c79a48a352dc
Murilo Zanini <45716780+murilofz@users.noreply.github.com>
2019-12-08 17:15:12 -0300

    Add files via upload
    Adicionado arquivo .py para entrega do projeto

---
+++ Projeto_final.py
@@ -0,0 +1,310 @@
+# In[1]:
+"""
+===============================================================================
+                        IMPORTING LIBRARIES
+===============================================================================
+"""
+# Importings base libraries
+import os;
+import pandas as pd; 
+import numpy as np;
+import librosa;
+
+# Models
+import sklearn.model_selection # train_test_split
+import sklearn.discriminant_analysis # LinearDiscriminantAnalysis
+import sklearn.naive_bayes  # GaussianNB
+import sklearn.ensemble
+
+seed = 42
+
+
+# In[2]:
+"""
+===============================================================================
+                                SETUP
+===============================================================================
+"""
+# String constants
+SAMPLE_RATE = "sr"
+LABEL = "label"
+DURACAO = "duracao"
+ARQUIVO_ORIGINAL = "original_file"
+
+
+# In[3]:
+"""
+===============================================================================
+                    DEFINING INGESTION FUNCIONS
+===============================================================================
+"""
+def _process_wave_file(wave_file, labels_list, filename_list, duration_list, sr_list, data_list, interval_time = 2):
+    ''' This function will append to the lists with the data from the wave file. 
+    It does not have a return'''
+    # Get data from the wave file:
+    audio_data, sampling_rate = librosa.load(wave_file, None)
+    original_filename = os.path.basename(wave_file)
+    original_filename = os.path.splitext(original_filename)[0]
+
+    # Calculate Some Attributes
+    labels = list(original_filename)[0:4] # each label contains 4 letters
+    frames_per_audio = sampling_rate * interval_time
+    
+    
+    # Separate the Wave File in interval_time sections.
+    rows_processed = 0
+    
+    for i, ini in enumerate(range(0, audio_data.shape[0], frames_per_audio)):
+            
+            # Calculate attributes
+            this_audio = pd.Series(audio_data[ini:(ini+frames_per_audio)])
+            this_duration = this_audio.shape[0]/sampling_rate
+            # Update the lists with this section data.
+            rows_processed += 1
+            filename_list.append(original_filename)
+            duration_list.append(this_duration)
+            sr_list.append(sampling_rate)
+            data_list.append(this_audio)
+            
+            
+   
+    # If we process more intervals than those predicted by our original_filename,
+    # We label as "?"
+    while(len(labels) < rows_processed):
+        #print(f"adding ? to {original_filename}")
+        labels.append("?")  
+    
+    # Update the labels list.
+    labels_list.extend(labels)
+
+def _load_wavs_from_dir(directory, verbose=False):
+    # Using those imports only on this function
+    from os.path import isfile, join
+    from os import listdir
+    
+    # Reading wave files from the directory
+    wave_files = [join(directory , f) for f in listdir(directory) if (isfile(join(directory, f)) and f.endswith(".wav")) ]
+    
+    # Creating lists that will store the data
+    labels_list = list()
+    filename_list = list() 
+    duration_list = list()
+    sr_list = list() 
+    data_list = list()
+    
+    # Auxiliar variables
+    processed = 1;   # For Verbose output
+    to_be_processed = len(wave_files) # For Verbose output
+    
+    for file in wave_files:
+        if(verbose): print(f"{file}: Processing {processed} of {to_be_processed}.")
+        _process_wave_file(file, labels_list, filename_list, duration_list, sr_list, data_list)
+        processed += 1
+    # After process all the files, create the DataFrame
+    if(verbose): print("Creating DataFrame")
+    df = pd.DataFrame(data_list)
+    if(verbose): print("Inserting Labels...")
+    df.insert(loc  = 0, column = LABEL, value = labels_list)
+    if(verbose): print("Inserting Duração...")
+    df.insert(loc  = 1, column = DURACAO, value = duration_list)
+    if(verbose): print("Inserting Sampling Rates(sr)...")
+    df.insert(loc = 2, column = SAMPLE_RATE, value = sr_list )
+    if(verbose): print("Inserting Original Filename...")
+    df.insert(loc = 3, column = ARQUIVO_ORIGINAL, value = filename_list)
+    if(verbose): print("DataFrame Created. Returning")
+    return(df)
+    
+    
+# In[4]:    
+"""
+===============================================================================
+                    DEFINING PIPELINE FUNCIONS
+===============================================================================
+"""            
+def load_data(data_directory, output_pickle_file = None, reuse_if_exists=True):
+    if(output_pickle_file):
+        output_extension = os.path.splitext(output_pickle_file)[1]
+        if (output_extension != ".pickle"):
+            raise("Output must be a file ended with .pickle") 
+    
+        if( reuse_if_exists and os.path.isfile(output_pickle_file) ):
+            # If the user wants to reuse existing pickle file and it exists
+            return pd.read_pickle(output_pickle_file)
+        
+        else:
+            # If the user do not wan´t to use existing file, or if it does not exists
+            df = _load_wavs_from_dir(data_directory)
+            df.to_pickle(output_pickle_file)
+            return df
+    return(_load_wavs_from_dir(data_directory))
+
+def preprocess_data(df):
+    ''' Filtre, remova nulls, e transforme os dados nessa etapa'''
+    prepared = df[df.label != "?"] # Remove rows with unknown labels
+    return prepared.fillna(0, inplace=False)
+
+
+def extract_features_old(df):
+    features = df.iloc[ : , df.columns.get_loc(0): ]    
+    return features
+
+def extract_features(df):
+    features = df.iloc[ : , df.columns.get_loc(0): ]
+    sample_rate = df.iloc[ : , SAMPLE_RATE]
+    mfccs     = np.mean(librosa.feature.mfcc(y=features, sr=sample_rate, n_mfcc=20).T, axis=0)
+    centroid  = librosa.feature.spectral_centroid(y = features, sr = sample_rate)
+    bandwidth = librosa.feature.spectral_bandwidth(y = features, sr = sample_rate)
+    flatness  = librosa.feature.spectral_flatness(y = features, sr = sample_rate)
+    rollof    = librosa.feature.spectral_rollof(y = features, sr = sample_rate)
+    tonnetz   = librosa.feature.tonnetz(y = features, sr = sample_rate)
+    zero_crossing = librosa.feature.zero_crossing_rate(y = features, sr = sample_rate)
+        
+    return features
+
+def extract_labels(df):
+    labels = df.loc[ : , LABEL]
+    
+    label_encoder = sklearn.preprocessing.LabelEncoder()
+    label_encoder.fit(["a", "b", "c", "d", "h", "m", "n", "x", "6", "7","?"])
+    labels = label_encoder.transform(labels)
+    return labels
+
+def score_classifier(df, y_pred):
+    prepared_data = preprocess_data(df)
+    y_real = extract_labels(prepared_data)
+    
+    print("Confusion Matrix:")
+    print(sklearn.metrics.confusion_matrix(y_true=y_real, y_pred = y_pred))
+    
+    print("\n Other Observations:")
+    
+    trues = y_real == y_pred
+    hits = sum(trues)
+    total = len(y_pred)
+    print(f"It got right: {hits} from {total} letters: {100*hits/total :.2f}%")
+    word_hits = prepared_data[trues].original_file.value_counts()
+    unique_words = len(df.original_file.unique())
+    print(f"It received {unique_words} words. ")
+    print(f"It got right 4 letters of: {sum(word_hits == 4)} words.\n" +
+          f"It got right 3 letters of: {sum(word_hits == 3)} words.\n" +
+          f"It got right 2 letters of: {sum(word_hits == 2)} words.\n" +
+          f"It got right 1 letters of: {sum(word_hits == 1)} words.\n" +
+          f"It got right 0 letters of: {unique_words - len(word_hits)} words.\n")
+    
+    print("Those are the words and hit count:")
+    print(word_hits)
+    print("Those are the letters:")
+    print(prepared_data[trues].label.value_counts())
+    
+  
+    
+# In[5]:   
+"""
+===============================================================================
+                    DEFINING MAIN FUNCIONS
+===============================================================================
+""" 
+def process_data(training_data, validation_data, algorithm):
+    # Preprocess - Filter and Imputing 
+    train_data = preprocess_data(training_data)
+    test_data  = preprocess_data(validation_data)
+    
+    # Extracting information
+    x_train = extract_features(train_data)
+    y_train = extract_labels(train_data)
+    
+    x_test = extract_features(test_data)
+    y_test = extract_labels(test_data)
+    
+    # Fit model
+    algorithm.fit(x_train, y_train)
+    
+    # Predict
+    predict_train = algorithm.predict(x_train)
+    predict_test  = algorithm.predict(x_test)
+    
+    score_classifier(validation_data, predict_test)
+    
+    return(predict_train, predict_test)
+    
+def process_folder(training_folder, validation_folder, algorithm):
+    ## Ler os dados
+    training_data = load_data(training_folder, verbose=True)
+    validation_data = load_data(validation_folder, verbose=True)
+    
+    return process_data(training_data, validation_data, algorithm)
+
+
+# In[6]:
+"""
+===============================================================================
+            DEFINING DATA PATH AND CREATING MODEL (LDA AND NAIVE BAYES)
+===============================================================================
+""" 
+
+# 
+## Inputs
+### In-paths
+train_path = ".\\dados\\TREINAMENTO\\"
+test_path  = ".\\dados\\VALIDACAO\\"
+
+### Out-paths -> Will be used to avoid having to reload all data
+train_pickle = ".\\dados\\treina_1752.pickle"
+test_pickle = ".\\dados\\valida_1752.pickle"
+
+## Algorithms
+lda = sklearn.discriminant_analysis.LinearDiscriminantAnalysis()
+nb = sklearn.naive_bayes.GaussianNB()
+
+
+# In[7]:
+"""
+===============================================================================
+                        LOADING DATA
+===============================================================================
+""" 
+
+training_data = load_data(train_path, train_pickle)
+validation_data = load_data(test_path, test_pickle)
+
+
+# In[8]:
+"""
+===============================================================================
+                        RUNNING MODEL (LDA)
+===============================================================================
+"""
+### Predict and scoring the validation
+train_predict_lda, test_predict_lda = process_data(training_data, validation_data, lda)
+
+
+## LDA
+### Scoring the training
+score_classifier(training_data, train_predict_lda)
+
+
+# In[9]:
+"""
+===============================================================================
+                        RUNNING MODEL (NB)
+===============================================================================
+"""
+## NB
+### Predict and scoring the validation
+train_predict_nb, test_predict_nb = process_data(training_data, validation_data, nb)
+
+
+### NB: Scoring the training
+score_classifier(training_data, train_predict_nb)
+
+
+# In[10]:
+"""
+===============================================================================
+                        RUNNING MODEL (RANDOM FOREST)
+===============================================================================
+"""
+## RF
+### Predict and scoring the validation
+rf = sklearn.ensemble.RandomForestClassifier(n_estimators = 100, max_depth = 10, min_samples_split = 5, random_state=seed, verbose=True)
+train_predict_rf, test_predict_rf = process_data(training_data, validation_data, rf)
